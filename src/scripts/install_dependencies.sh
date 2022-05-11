01347a780a8534d42ce7c9292766015f33e40391
Rodrigo Neder <rodrigo.neder@protonmail.com>
2022-05-11 15:21:35 +0000

    modified CodeDeploy scripts

--- src/scripts/install_dependencies.sh
+++ src/scripts/install_dependencies.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
 dependencies(){
-    sudo dnf install npm -y
-    cd src || exit 1
-    npm install
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    . $HOME/.nvm/nvm.sh
+    nvm install 16 # latest LTS version
+    node -v
+    npm -v
 }
 
 dependencies
--- src/scripts/start_application.sh
+++ src/scripts/start_application.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 
-pm2 start ecosystem.config.js 
+install_pm2_start_app(){
+    npm -i g pm2
+    pm2 -v
+
+    pm2 start ecosystem.config.js
+}
+
+install_pm2_start_app
