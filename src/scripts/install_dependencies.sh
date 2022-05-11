efc79ca6e73d4bc84bfc363b1e657aa310f8ec7e
Rodrigo Neder <rodrigo.neder@protonmail.com>
2022-05-11 15:39:45 +0000

    changed node version

--- src/scripts/install_dependencies.sh
+++ src/scripts/install_dependencies.sh
@@ -2,7 +2,7 @@
 
 dependencies(){
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-    . $HOME/.nvm/nvm.sh
+    . "$HOME"/.nvm/nvm.sh
     nvm install 16 # latest LTS version
     node -v
     npm -v
