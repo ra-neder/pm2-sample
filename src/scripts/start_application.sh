8cd397deb6a84a89bd235bb283d9435f950241e3
Rodrigo Neder <rodrigo.neder@protonmail.com>
2022-05-11 16:00:40 +0000

    Trying ONCE AGAIN to deal with command paths

--- src/scripts/start_application.sh
+++ src/scripts/start_application.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env sh
 
 install_pm2_start_app(){
-    echo "$(which npm)"
-    echo "$(whoami)"
-    echo "$(ls $HOME/.nvm)"
+    . "$HOME"/.bashrc
+    npm -i g pm2
+    pm2 -v
 
-#    npm -i g pm2
-#    pm2 -v
-#
-#    pm2 start ecosystem.config.js
+    pm2 start ecosystem.config.js
 }
 
 install_pm2_start_app
